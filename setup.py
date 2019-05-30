6ae81b116d461ef2a7a71181ccb7ac2083a7fa04
saif-sultan <saif.sultan2@gmail.com>
2019-05-30 12:32:35 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -17,57 +17,6 @@ from codecs import open
 from os import path
 here = path.abspath(path.dirname(__file__))
 
-
-def discover_and_run_tests():
-    import os
-    import sys
-    import unittest
-
-    # get setup.py directory
-    setup_file = sys.modules['__main__'].__file__
-    setup_dir = os.path.abspath(os.path.dirname(setup_file))+"/tests/unittests"
-
-    # use the default shared TestLoader instance
-    test_loader = unittest.defaultTestLoader
-
-    # use the basic test runner that outputs to sys.stderr
-    test_runner = unittest.TextTestRunner()
-
-    # automatically discover all tests
-    # NOTE: only works for python 2.7 and later
-    test_suite = test_loader.discover(setup_dir, pattern='unittest*.py')
-
-    # run the test suite
-    test_runner.run(test_suite)
-
-try:
-    from setuptools.command.test import test
-
-    class DiscoverTest(test):
-
-        def finalize_options(self):
-            test.finalize_options(self)
-            self.test_args = []
-            self.test_suite = True
-
-        def run_tests(self):
-            discover_and_run_tests()
-
-except ImportError:
-    from distutils.core import Command
-
-    class DiscoverTest(Command):
-        user_options = []
-
-        def initialize_options(self):
-                pass
-
-        def finalize_options(self):
-            pass
-
-        def run(self):
-            discover_and_run_tests()
-
 # Get the long description from the relevant file
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
